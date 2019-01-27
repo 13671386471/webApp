90f6b6b4024468a260bc5fb63a0f37455a335193
liqiankun <13671386471.com>
2019-01-27 22:03:13 +0800

    '数据库连接‘'

---
+++ server/server.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let mongoose = require('mongoose');
+//连接mongodb数据库；并且使用imooc这个集合
+const DB_URL = 'mongodb://localhost:27017/imooc';
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected', function () {
+    console.log('mongodb connect successful');
+});
+//连接mongoodb数据库
+//定义数据模型;mongoose.modal和mongoose.Schema的应用
+let User = mongoose.model("user", new mongoose.Schema({
+    user:{type: String, require: true},
+    age: {type: Number, require: true}
+}))
+//定义数据模型
+
+//根据定义的数据模型生成数据【开始】
+User.create({
+    user: 'react',
+    age: 3
+}, function (err, response) {
+    console.log('mongoodb Data::::', response);
+})
+//根据定义的数据模型生成数据【结束】
+let app = express();
+
+app.get('/',function(require, response){
+    response.send('<h2>express and react</h2>');
+});
+
+app.get('data', function (require, response) {
+    response.json({name: 'react', type: 'It'})
+});
+
+
+app.listen(9099,function(){
+    console.log('express start at port 9099');
+})
+
+
+
+
+
+
